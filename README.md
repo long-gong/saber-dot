f117d1b1e6107ade1ccec341581d5b02ad9d32ba
Long Gong (gtuser) <gonglong@gatech.edu>
2019-02-01 12:04:36 -0500

    update README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # saber-dot
 dot files for saber
+
+
+## How to Use Them
+
+```bash
+# Clone this repo
+git clone https://github.com/long-gong/saber-dot.git
+# cd to it
+cd saber-dot
+# Clone all submodules for vim
+git submodule update --init --recursive
+# Clone all submodules for zsh
+cd script && ./clone_nested_submodules.py
+# Handle special submodules
+cd ../.vim/bundle/YouCompleteMe
+# Please make sure you have installed all dependencies for YouCompleteMe
+# before executing the following commands, which can be found at
+# https://github.com/Valloric/YouCompleteMe
+python install.py --clang-completer
+# Backup your old dot files (if you have them)
+mv ~/.vimrc ~/.vimrc.OLD
+mv ~/.vim ~/.vim.OLD
+mv ~/.zshrc ~/.zshrc.OLD
+mv ~/.oh-my-zsh ~/.oh-my-zsh.OLD
+# Move those dot files to you home directory
+mv .vimrc .vim ~
+mv .zshrc .oh-my-zsh ~
+```
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,41 @@
 #!/usr/bin/env bash
 
 set -e
+echo "Clone all submodules for vim"
 cd "$(dirname "$0")"/..
 git submodule update --init --recursive
 
+echo "Install dependencies for YouCompleteMe"
+cd .vim/bundle/YouComplete 
+python install.py --clang-completer
+
+echo "Clone all submodule for oh-my-zsh manually"
 cd "$(dirname "$0")"
 chmod +x ./clone_nested_submodules.py
 ./clone_nested_submodules.py
+
+echo "Start to move those dot files into ~"
+## declare an array variable
+declare -a vim_dot_files=(".vimrc" ".vim")
+declare -a zsh_dot_files=(".zshrc" ".oh-my-zsh")
+
+function just_move_it(){
+	arr=$1
+	dest="$HOME"
+	for f in "${arr[@]}" 
+	do 
+		echo "Try to Move ${f}"
+		if [ -f "$dest/$f" ] then
+			echo "${dest}/${f} already exists, are you sure to move (we will create a backup for your original files)? [Y/N]"
+			read response 
+			if [ "${response}" == 'Y' ] 
+}
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   echo "$i"
+   # or do whatever with individual element of the array
+done
 
 
