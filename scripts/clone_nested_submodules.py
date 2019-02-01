82e61076f4ebde58c8f27323dda7e4602137b46e
Long Gong (gtuser) <gonglong@gatech.edu>
2019-02-01 11:32:04 -0500

    add setup.sh

--- scripts/clone_nested_submodules.py
+++ scripts/clone_nested_submodules.py
@@ -4,17 +4,14 @@ import configparser
 import os
 
 if __name__ == '__main__':
-config = configparser.ConfigParser()
-
-config.read('submodules')
-
-print(config.sections())
-
-for sec in config.sections():
-    try:
-        process = Popen(['git',  'clone', config[sec]['url']], stdout=PIPE,
-                        stderr=PIPE, cwd=os.path.normalpath(os.path.join('..', config[sec]['path'])))
-        stdout, stderr = process.communicate()
-        print(stdout)
-    except:
-        pass
+    config = configparser.ConfigParser()
+    config.read('submodules')
+    for sec in config.sections():
+        cmd = ['git',  'clone', config[sec]['url'], os.path.normpath(os.path.join('..', config[sec]['path']))]
+        print(' '.join(cmd)) 
+        try:
+            process = Popen(cmd, stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process.communicate()
+            print(stdout)
+        except Exception as e:
+            print('failed: ' + e.message)
